ad8447ba1dda98f93f550b6c6929007133a6bd57
Preston Chapman <pchapman82070@uat.edu>
2024-08-19 10:30:41 -0700

    Begin Timer

--- Main.java
+++ Main.java
@@ -1,6 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        Timer t = new Timer(10000);
+        Thread thread1 = new Thread(t);
+        thread1.start();
+        // Get User Input
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        thread1.interrupt();
+        System.out.println("You entered: " + userInput);
+        System.out.println("Timer is: "+ t.counter);
+        
+    }
+}
+
+class Timer implements Runnable {
+    public final int lengthMS;
+    public int counter = 0;
+    public Timer(int lengthMS){
+        this.lengthMS = lengthMS;
+    }
+    @Override
+    public void run() {
+        for(int i=0; i<lengthMS; i++){
+            try {
+                Thread.sleep(1);
+                counter++;
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+        System.out.println("Timer finished.");
     }
 }
 
