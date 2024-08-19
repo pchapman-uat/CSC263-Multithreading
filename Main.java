cefb466d643066c10651f4d21370fcac3c3ddd8b
Preston Chapman <pchapman82070@uat.edu>
2024-08-19 10:44:40 -0700

    Change length to goal, added end amount, and new user input

--- Main.java
+++ Main.java
@@ -5,10 +5,18 @@ public class Main {
     static boolean timerFinished = false;
     public static void main(String[] args){
         System.out.println("Hello World!");
-        Timer t = new Timer(1000);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many miliseconds do you want to count?");
+        int timer = scanner.nextInt();
+        System.out.println("How late can you be?");
+        int end = scanner.nextInt();
+
+
+        Timer t = new Timer(timer, end);
+
         Thread thread1 = new Thread(t);
         thread1.start();
-        Scanner scanner = new Scanner(System.in);
+
         while(!timerFinished){
             try {
                 if (System.in.available() > 0) {
@@ -25,14 +33,17 @@ public class Main {
 }
 
 class Timer implements Runnable {
-    public final int lengthMS;
+    public final int gaol;
+    public final int end;
     public int counter = 0;
-    public Timer(int lengthMS){
-        this.lengthMS = lengthMS;
+    
+    public Timer(int gaol, int end){
+        this.gaol = gaol;
+        this.end = end;
     }
     @Override
     public void run() {
-        for(int i=0; i<lengthMS; i++){
+        for(int i=0; i<gaol+end; i++){
             try {
                 Thread.sleep(1);
                 counter++;
