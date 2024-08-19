a4a752a525fb2a4dc305549eba1362ef3057a225
Preston Chapman <pchapman82070@uat.edu>
2024-08-19 10:22:16 -0700

    Add Runnable

--- Main.java
+++ Main.java
@@ -4,9 +4,17 @@ public class Main {
         MyThread t1 = new MyThread("Thread 1");
         MyThread t2 = new MyThread("Thread 2");
         MyThread t3 = new MyThread("Thread 3");
+
+        Thread t4 = new Thread(new MyRunnable("Thread 4"));
+        Thread t5 = new Thread(new MyRunnable("Thread 5"));
+        Thread t6 = new Thread(new MyRunnable("Thread 6"));
+        
         t1.start();
         t2.start();
         t3.start();
+        t4.start();
+        t5.start();
+        t6.start();
     }
 }
 
@@ -25,5 +33,24 @@ class MyThread extends Thread {
             }
         }
         System.out.println(threadName + " finished.");
+    }
+}
+
+class MyRunnable implements Runnable {
+    private String threadName;
+    public MyRunnable(String name) {
+        threadName = name;
+    }
+
+    @Override
+    public void run(){
+        for(int i=0;i<5;i++){
+            System.out.println(threadName + " is running- "+i);
+            try  {
+                Thread.sleep(1000);
+            } catch(InterruptedException e) {
+                System.out.println( threadName + " interrupted.");
+            }
+        }
     }
 }
