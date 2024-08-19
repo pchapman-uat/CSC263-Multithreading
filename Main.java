25fdafe02e72eaee667c88a04337b5d32ba84553
Preston Chapman <pchapman82070@uat.edu>
2024-08-19 10:18:16 -0700

    Add Thread Class

--- Main.java
+++ Main.java
@@ -3,3 +3,21 @@ public class Main {
         System.out.println("Hello World!");
     }
 }
+
+class MyThread extends Thread {
+    private String threadName;
+    public MyThread(String name) {
+        threadName = name;
+    }
+    public void run(){
+        for(int i=0; i<5; i++){
+            System.out.println(threadName + " is running- "+i);
+            try  {
+                Thread.sleep(1000);
+            } catch(InterruptedException e) {
+                System.out.println( threadName + " interrupted.");
+            }
+        }
+        System.out.println(threadName + " finished.");
+    }
+}
